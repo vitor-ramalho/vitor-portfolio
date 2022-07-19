61afc77fbdedb62b2fe0d4ea327e172ff6264134
{"nickname": "JSON"} <39497088+vitor-ramalho@users.noreply.github.com>
2022-07-19 12:12:42 -0300

    Home change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-[#00D1FF]'>Olá! Me chamo</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Vitor Ramalho Vilela</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Eu sou um desenvolvedor Front End</h2>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Desenvolvedor Full Stack</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           Seja bem vindo ao meu site! Aqui irei exibir minha experiência como desenvolvedor, tecnologias já utilizadas e alguns projetos já desenvolvidos.
         </p>
