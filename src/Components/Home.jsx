1805f9a65c2a1fa5e7f3d20cdd295175830ac0ff
{"nickname": "JSON"} <39497088+vitor-ramalho@users.noreply.github.com>
2022-07-21 10:28:40 -0300

    Home change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/**Container*/}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#00D1FF]'>Olá! Me chamo</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Vitor Ramalho Vilela</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Desenvolvedor Full Stack</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
